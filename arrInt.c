5b4dafa0b842136875132edbdb157f34a39d8f54
nhthucdtvt <nhthuc.dtvt@gmail.com>
2019-04-05 08:58:56 +0700

    Update arrInt.c

--- arrInt.c
+++ arrInt.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void ktArr(int *arr, int n);
+void ArrChk(int *arr, int n);
 
 int main(int argc, char const *argv[])
 {
@@ -19,30 +19,27 @@ int main(int argc, char const *argv[])
 		scanf("%d",&arr[i]);
 		printf("gia tri vua nhap:%d\n", arr[i]);
 	}
-	
-	ktArr(arr,n);
+
+	ArrChk(arr,n);
 
 	return 0;
 }
 
-void ktArr(int *arr, int n){
-	
-	int kq[20], k = 0, tick = 0;
+void ArrChk(int *arr, int n){
+	int kq[20], k = 0;
 
 	for (int i = 1; i <= n; i++)
 	{
 		for (int j= 0; j < n; j++)
 		{
 			if(i == arr[j]){
-				tick = 1;
-				break;	
-			} 
-			if((j == n-1) && (tick == 0)){
+				break;
+			}
+			if((j == n-1) && (i != arr[j])){
 				kq[k] = i;
 				printf("kq:%d\n", kq[k]);
 				break;
-			}	
-		}			
-		tick = 0;				
-	}	
+			}
+		}
+	}
 }
