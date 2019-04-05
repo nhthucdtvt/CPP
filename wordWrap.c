1397fb5e8e74e68204b6888cee2e0a68293760ea
nhthucdtvt <nhthuc.dtvt@gmail.com>
2019-04-05 08:52:56 +0700

    Update wordWrap.c

--- wordWrap.c
+++ wordWrap.c
@@ -9,6 +9,8 @@ int main(int argc, char const *argv[])
 {
 	/* code */
 	char *str = (char*)malloc(20*sizeof(char));
+	char *str2 = (char*)malloc(20*sizeof(char));
+
 	printf("nhap chuoi:\n");
 	scanf("%[^\n]",str);
 	printf("chuoi vua nhap:%s\n", str);
@@ -32,34 +34,14 @@ int main(int argc, char const *argv[])
 			end = i;
 			printf("end:%d\n", end);
 			break;
-		} 
+		}
 	}
 
-	char *str1 = (char*)malloc(20*sizeof(char));
-	char *str2 = (char*)malloc(20*sizeof(char));
-	char *str3 = (char*)malloc(20*sizeof(char));
-	char *str4 = (char*)malloc(20*sizeof(char));
-	char *kq = (char*)malloc(200*sizeof(char));
-
-
-	memcpy(str1,str,beg+1);
-	memcpy(str3,str+end,strlen(str)-1);
 	memcpy(str2,str+beg+1,end-beg-1);
+	str2 = strrev(str2);
 
-	// printf("str1:%s\n", str1);
-	// printf("str2:%s\n", str2);
-	// printf("str3:%s\n", str3);
-
-	str4 = strrev(str2);
-	kq = strcat(str1,str4);
-	printf("kq:%s\n", kq);
-	kq = strcat(kq,str3);
-	printf("kq:%s\n", kq);
-
-	free(str1);
-	free(str2);
-	free(str3);
-	free(str4);
+	memcpy(str+beg+1,str2,strlen(str2));
+	printf("str:%s\n", str);
 
 	return 0;
 }
